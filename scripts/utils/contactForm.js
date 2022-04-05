2b53fe652fbb828758818fff7f158f80f51367cd
Anthony COLAS <anthonycolas.france@gmail.com>
2022-04-05 10:59:43 +0200

    modal accessible OK

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,11 +1,24 @@
+// Functions
 function displayModal() {
+    document.getElementById('main-wrapper').setAttribute('aria-hidden', 'true');
+    document
+        .getElementById('contact_modal')
+        .setAttribute('aria-hidden', 'false');
     const modal = document.getElementById('contact_modal');
     modal.style.display = 'block';
+    document.querySelector('.contact_button').focus();
 }
 
 function closeModal() {
+    document
+        .getElementById('main-wrapper')
+        .setAttribute('aria-hidden', 'false');
+    document
+        .getElementById('contact_modal')
+        .setAttribute('aria-hidden', 'true');
     const modal = document.getElementById('contact_modal');
     modal.style.display = 'none';
+    document.querySelector('.modal-close-btn').focus();
 }
 
 function submitInfo() {
@@ -19,3 +32,18 @@ function submitInfo() {
         console.log(entry);
     }
 }
+
+const modal = document.getElementById('contact_modal');
+
+window.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        document
+            .getElementById('main-wrapper')
+            .setAttribute('aria-hidden', 'false');
+        document
+            .getElementById('contact_modal')
+            .setAttribute('aria-hidden', 'true');
+        modal.style.display = 'none';
+        document.querySelector('.modal-close-btn').focus();
+    }
+});
